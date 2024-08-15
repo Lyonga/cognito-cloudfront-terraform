58ac57542ab2f97378eff998969e11b057bc6ffb
clyonga <charles.lyonga1@accesscfa.com>
2024-08-15 12:57:46 -0400

    recreate everything

--- infra/main.tf
+++ infra/main.tf
@@ -212,22 +212,6 @@ resource "aws_wafv2_web_acl" "cf_web_acl" {
     }
   }
 }
-# data "aws_iam_policy_document" "s3_bucket_policy" {
-# statement {
-#     actions   = ["s3:GetObject"]
-#     resources = ["aws_s3_bucket.s3-static-website.arn/*"]
-#     principals {
-#     type        = "Service"
-#     identifiers = ["cloudfront.amazonaws.com"]
-#     }
-#     condition {
-#     test     = "StringEquals"
-#     variable = "AWS:SourceArn"
-#     values   =  [aws_cloudfront_distribution.cf-dist.arn] 
-#     }
-# }
-# }
-
 resource "aws_s3_bucket_policy" "static_site_bucket_policy" {
 bucket = aws_s3_bucket.s3-static-website.id
 #policy = data.aws_iam_policy_document.s3_bucket_policy.json
