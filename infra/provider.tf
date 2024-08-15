4d852e19569dc7eaf2ad77c6ec04438146892648
clyonga <charles.lyonga1@accesscfa.com>
2024-08-15 09:51:48 -0400

    removed aws secrets

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,7 +1,3 @@
 provider "aws" {
   region = "us-east-1"
-#   access_key = "AKIAY5NZHLQ6663RQPH6"
-#   secret_key = "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"
-  access_key = "AKIAY5NZHLQ6STMJQP2E"
-  secret_key = "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"
 }
