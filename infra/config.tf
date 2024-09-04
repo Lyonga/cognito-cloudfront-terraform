3ff043f37d8fc8be0f963c15f54bb5959c59e757
clyonga <charles.lyonga1@accesscfa.com>
2024-09-04 14:43:47 -0400

    config typo

--- infra/config.tf
+++ infra/config.tf
@@ -25,9 +25,8 @@ resource "aws_lambda_function" "check_ami" {
   filename         = data.archive_file.lambda_package.output_path  # Points to the zip archive
   function_name    = "terminate_non_compliant_instance"
   role             = aws_iam_role.lambda_exec.arn
-  handler          = "lambda_function.lambda_handler"
+  handler          = "test.lambda_handler"  # Specify the Python file and function name
   runtime          = "python3.8"
-  source_code_hash = filebase64sha256("lambda_function.zip")
 
   environment {
     variables = {
